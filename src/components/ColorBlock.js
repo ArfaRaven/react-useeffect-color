90195c68c937234e8995871bc60bea0af6302bb8
Anastasiia <153719547+ArfaRaven@users.noreply.github.com>
2026-04-13 22:53:54 +0300

    Add files via upload

---
+++ src/components/ColorBlock.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+function ColorBlock() {
+
+  const [color, setColor] = useState("red");
+
+  const changeColor = () => {
+    setColor(color === "red" ? "blue" : "red");
+  };
+
+  useEffect(() => {
+    console.log("Поточний колір:", color);
+  }, [color]);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          backgroundColor: color,
+          margin: "20px auto"
+        }}
+      ></div>
+
+      <button onClick={changeColor}>
+        Змінити колір
+      </button>
+
+    </div>
+  );
+}
+
+export default ColorBlock;
